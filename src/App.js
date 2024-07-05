f7461c43508af74a13fe4a712350d150c42cfe27
moresh <moreshsatpute@gmail.com>
2024-07-05 19:27:33 +0530

    headerstyle update

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@
 import React from 'react'
 import {BrowserRouter as Router, Routes,Route} from 'react-router-dom'
 import Home from './pages/Home'
-import Header from './components/Header'
+// import Header from './components/Header'
 
 const App = () => {
   return (
         <Router>
-          <Header/>
+          {/* <Header/> */}
             <Routes>
                 <Route  path='/' element={<Home/>} />
             </Routes>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,44 +1,62 @@
 import React from 'react'
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+// import Container from 'react-bootstrap/Container';
+import { Nav, Navbar, Container } from 'react-bootstrap';
+// import Navbar from 'react-bootstrap/Navbar';
+
+// import NavDropdown from 'react-bootstrap/NavDropdown';
+import Logo from "../assets/logo/logo.png"
+import { Link, } from 'react-router-dom';
+import './HeaderStyle.css';
+
+
 
 const Header = () => {
   return (
-        <Navbar  >
-          <Container>
-            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-              <Nav className="me-auto">
-                <Nav.Link href="#features">Features</Nav.Link>
-                <Nav.Link href="#pricing">Pricing</Nav.Link>
-                <NavDropdown title="Dropdown" id="collapsible-nav-dropdown">
-                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">
-                    Another action
-                  </NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action/3.4">
-                    Separated link
-                  </NavDropdown.Item>
-                </NavDropdown>
-              </Nav>
-              <Nav>
-                <Nav.Link href="#deets">More deets</Nav.Link>
-                <Nav.Link eventKey={2} href="#memes">
-                  Dank memes
-                </Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
-      );
-    }
-    
-  
-  
+    <header>
+
+    <Navbar  >
+      <Container>
+        <Navbar.Brand href="#home">
+          <Link><img src={Logo} alt="Logo" className='img-fluid' /></Link>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link as={Link} to="/">
+              HOME
+            </Nav.Link>
+            <Nav.Link as={Link} to="/About">
+              ABOUT
+            </Nav.Link>
+            <Nav.Link as={Link} to="/Menu">
+              OUR MENU
+            </Nav.Link>
+            <Nav.Link as={Link} to="/Shop">
+              SHOP
+            </Nav.Link>
+            <Nav.Link as={Link} to="/Blog">
+              BLOG
+            </Nav.Link>
+            <Nav.Link as={Link} to="/Contact">
+              CONTACT
+            </Nav.Link>
+            <Nav.Link as={Link} to="/Contact">
+            <div className='cart'>
+
+              <i class="bi bi-bag fs-5"></i>
+              <em className='roundpoint'>2</em>
+            </div>
+            </Nav.Link>
+
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    </header>
+  );
+}
+
+
+
 
 export default Header
