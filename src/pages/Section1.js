1b870c7426c801a47ff3a2715103895952bc8d3f
moresh <moreshsatpute@gmail.com>
2024-08-23 15:16:36 +0530

    image change

--- src/pages/Section1.js
+++ src/pages/Section1.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import Burger from "../assets/hero/hero-2.png";
+import ChickenHandi from "../assets/hero/hero-2.png";
 import { Link } from "react-router-dom";
 
 const Section1 = () => {
@@ -10,7 +10,7 @@ const Section1 = () => {
         <Row>
           <Col lg={7} className="mb-5 mb-lg-0">
             <div className="position-relative">
-              <img src={Burger} className="img-fluid" alt="Hero" />
+              <img src={ChickenHandi} className="img-fluid" alt="Hero" />
               <div className="price_badge">
                 <div className="badge_text">
                   <h4 className="h4_xs">Only</h4>
@@ -21,7 +21,7 @@ const Section1 = () => {
           </Col>
           <Col lg={5}>
             <div className="hero_text text-center">
-              <h1 className="text-white">New Burger</h1>
+              <h1 className="text-white">Chicken Biryani</h1>
               <h2 className="text-white">With Onion</h2>
               <p className="text-white pt-2 pb-4">
                 Feugiat primis ligula risus auctor laoreet augue egestas mauris
